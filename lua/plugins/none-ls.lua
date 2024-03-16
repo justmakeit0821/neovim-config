a1135436f1d9af0818d34beb5f3261fd6c992259
Allen <allenho1208@gmail.com>
2024-03-16 23:21:45 +0800

    [Error Fixing] null-ls failed to load builtin eslint_d for method diagnostics

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,19 @@
 return {
 	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 
 		null_ls.setup({
 			sources = {
-                -- for formatting lua
+				-- for formatting lua
 				null_ls.builtins.formatting.stylua,
-                -- for formatting javascript
+				-- for formatting javascript
 				null_ls.builtins.formatting.prettier,
-                -- for linting javascript
-				null_ls.builtins.diagnostics.eslint_d
+				-- for linting javascript
+				require("none-ls.diagnostics.eslint_d"),
 			},
 		})
 
