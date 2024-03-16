0890d6e08df2f1d72bf6f4ed28f3e2b0bdbf2267
Allen <allenho1208@gmail.com>
2024-03-16 16:06:21 +0800

    [Keymap] <C-f> for global search (live_grep)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,7 +9,7 @@ return {
 
             -- set key bindings for Telescope functions
             vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
         end
     },
     {
