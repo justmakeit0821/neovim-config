0cc0eab350b1481921b4c914c74212e3cc543d10
Allen <allenho1208@gmail.com>
2024-03-16 16:08:22 +0800

    [Treesitter Config] auto_install treesitter parser for new filetypes encountered

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,9 +6,9 @@ return {
         local configs = require("nvim-treesitter.configs")
         configs.setup({
             ensure_installed = {"lua", "javascript"},
+            auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
         })
     end
 }
- 
