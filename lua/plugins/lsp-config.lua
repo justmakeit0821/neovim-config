58f7915a6a82d41f04a8c28b7ce648d19b7093d5
Allen <allenhoproject@gmail.com>
2024-04-02 22:28:00 +0800

    [EP 5] Enable Auto-completion With Code Snippets From LuaSnip & LSP; A Greeter UI

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,33 +1,41 @@
 return {
-    {
-        -- for installing language servers
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        -- for bridging mason with nvim-lspconfig, making it easier to use both plugins together
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver" }
-            })
-        end
-    },
-    {
-        -- for hooking up neovim to language servers
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            -- set up for each installed language server manually
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({}) -- prerequisite: npm install -g typescript (assume installed node.js + npm)
+	{
+		-- for installing language servers
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		-- for bridging mason with nvim-lspconfig, making it easier to use both plugins together
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver" },
+			})
+		end,
+	},
+	{
+		-- for hooking up neovim to language servers
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
 
-            -- set up lsp keymaps
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+			-- set up for each installed language server manually
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+			}) -- prerequisite: npm install -g typescript (assume installed node.js + npm)
+
+			-- set up lsp keymaps
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
