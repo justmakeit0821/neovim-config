0d9efd0fac20539a766ae90a4367c811c19eea4e
Allen <allenhoproject@gmail.com>
2024-04-10 21:33:14 +0800

    [Nvim Setup] break clipboard silo between Neovim and outside Neovim

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -8,3 +8,6 @@ vim.cmd("set number")
 -- set leader key to space
 vim.g.mapleader = " "
 vim.api.nvim_set_keymap("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+
+-- break clipboard silo between Neovim and outside Neovim
+vim.cmd("set clipboard+=unnamedplus")
