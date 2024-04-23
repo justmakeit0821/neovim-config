5a8164b3e9cda8369ddcc8172fc8e527c5fc3268
Allen <allenhoproject@gmail.com>
2024-04-23 21:18:19 +0800

    [Nvim Setup] set Search ignore cases

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,6 +4,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
+vim.cmd("set ignorecase")
 
 -- set leader key to space
 vim.g.mapleader = " "
