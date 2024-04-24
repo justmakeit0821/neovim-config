f115504c9f163968262a73cda176a1e843877dc0
Allen <allenhoproject@gmail.com>
2024-04-24 22:14:33 +0800

    [Plugin] add "rainbow-delimiters" for colored brackets; "hlchunk" for guided indentation

--- lua/plugins/code-helper.lua
+++ lua/plugins/code-helper.lua
@@ -1,3 +1,11 @@
 return {
-    { "RRethy/vim-illuminate" }
+    { "RRethy/vim-illuminate" },
+    { "HiPhish/rainbow-delimiters.nvim" },
+    {
+        "shellRaining/hlchunk.nvim",
+        event = { "UIEnter" },
+        config = function()
+            require("hlchunk").setup({})
+        end
+    },
 }
