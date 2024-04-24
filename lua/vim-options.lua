ac2d05fecb6cbc55dfb2ce57bcb45b33d5fd3df8
Allen <allenhoproject@gmail.com>
2024-04-24 22:11:36 +0800

    [Nvim Setup] set both number & relative number

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
+vim.cmd("set number relativenumber")
 vim.cmd("set ignorecase")
 
 -- set leader key to space
