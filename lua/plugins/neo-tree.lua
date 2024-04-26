1a98f9b16bdc5100687710d3ced8c44d369fda41
Allen <allenhoproject@gmail.com>
2024-04-27 00:11:33 +0800

    [image] adding missing property "kitty_method"

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -17,6 +17,7 @@ return {
                 end
                 require("image").setup({
                     backend = "kitty",
+                    kitty_method = "normal",
                     integrations = {
                         markdown = {
                             enabled = true,
