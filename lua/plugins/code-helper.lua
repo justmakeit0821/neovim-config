fb81156e16ca4b060165b2eced9cfd16e9c5d263
Allen <allenhoproject@gmail.com>
2024-04-28 23:14:57 +0800

    [Plugin] add "nvim-surround" & "mini.pairs" for automatic brackets pairing

--- lua/plugins/code-helper.lua
+++ lua/plugins/code-helper.lua
@@ -6,6 +6,21 @@ return {
         event = { "UIEnter" },
         config = function()
             require("hlchunk").setup({})
-        end
+        end,
+    },
+    {
+        "kylechui/nvim-surround",
+        version = "*",
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({})
+        end,
+    },
+    {
+        "echasnovski/mini.pairs",
+        version = "*",
+        config = function()
+            require("mini.pairs").setup()
+        end,
     },
 }
