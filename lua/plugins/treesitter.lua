f9c3d1009144a48bb0ebe4da961334c0ec72c2f6
Allen <allenhoproject@gmail.com>
2024-04-30 22:39:55 +0800

    [Plugin] add "log-highlight" for log file syntax highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,22 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        -- add TreeSitter for syntax highlighting
-        local configs = require("nvim-treesitter.configs")
-        configs.setup({
-            ensure_installed = { "lua", "javascript", "vim", "regex", "bash", "markdown", "markdown_inline" },
-            auto_install = true,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            -- add TreeSitter for syntax highlighting
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                ensure_installed = { "lua", "javascript", "vim", "regex", "bash", "markdown", "markdown_inline" },
+                auto_install = true,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+    {
+        "fei6409/log-highlight.nvim",
+        config = function()
+            require("log-highlight").setup({})
+        end,
+    },
 }
