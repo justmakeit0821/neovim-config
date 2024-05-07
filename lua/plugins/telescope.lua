3ad5055121c53aa8a0293060bb9634e5036d8805
Allen <allenhoproject@gmail.com>
2024-05-07 23:38:42 +0800

    [Telescope] change default layout_strategy to "vertical"

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,47 +1,51 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = {
-			{ "nvim-lua/plenary.nvim" },
-			{
-				"nvim-telescope/telescope-live-grep-args.nvim",
-				version = "^1.0.0",
-			},
-		},
-		config = function()
-			require("telescope").setup({
-				pickers = {
-					find_files = {
-						path_display = { "smart" },
-					},
-				},
-				extensions = {
-					live_grep_args = {
-						path_display = { "smart" },
-					},
-				},
-			})
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = {
+            { "nvim-lua/plenary.nvim" },
+            {
+                "nvim-telescope/telescope-live-grep-args.nvim",
+                version = "^1.0.0",
+            },
+        },
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    layout_strategy = "vertical",
+                    layout_config = { height = 0.9, mirror = true, prompt_position = "bottom" },
+                },
+                pickers = {
+                    find_files = {
+                        path_display = { "smart" },
+                    },
+                },
+                extensions = {
+                    live_grep_args = {
+                        path_display = { "smart" },
+                    },
+                },
+            })
 
-			require("telescope").load_extension("live_grep_args")
-			local builtin = require("telescope.builtin")
+            require("telescope").load_extension("live_grep_args")
+            local builtin = require("telescope.builtin")
 
-			-- set key bindings for Telescope functions
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<C-f>", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+            -- set key bindings for Telescope functions
+            vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+            vim.keymap.set("n", "<C-f>", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
