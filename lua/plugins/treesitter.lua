eaac9eb734646232b53fbcb439a7fd6c3537139d
Allen <allenhoproject@gmail.com>
2024-05-13 22:09:36 +0800

    [TreeSitter] add "vimdoc" to "ensure_installed"

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
             -- add TreeSitter for syntax highlighting
             local configs = require("nvim-treesitter.configs")
             configs.setup({
-                ensure_installed = { "lua", "javascript", "vim", "regex", "bash", "markdown", "markdown_inline" },
+                ensure_installed = { "lua", "javascript", "vim", "vimdoc", "regex", "bash", "markdown", "markdown_inline" },
                 auto_install = true,
                 highlight = { enable = true },
                 indent = { enable = true },
