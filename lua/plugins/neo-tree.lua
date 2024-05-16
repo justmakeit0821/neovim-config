2eb1e00589dfe0f187eb9e8e6cdc21ca7f3a811d
Allen <allenhoproject@gmail.com>
2024-05-16 21:32:14 +0800

    [NeoTree] enable copy filepaths

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -38,10 +38,10 @@ return {
                     max_height = nil,
                     max_width_window_percentage = nil,
                     max_height_window_percentage = 50,
-                    window_overlap_clear_enabled = false,                      -- toggles images when windows are overlapped
+                    window_overlap_clear_enabled = false,                                     -- toggles images when windows are overlapped
                     window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-                    editor_only_render_when_focused = false,                   -- auto show/hide images when the editor gains/looses focus
-                    tmux_show_only_in_active_window = false,                   -- auto show/hide images in the correct Tmux window (needs visual-activity off)
+                    editor_only_render_when_focused = false,                                  -- auto show/hide images when the editor gains/looses focus
+                    tmux_show_only_in_active_window = false,                                  -- auto show/hide images in the correct Tmux window (needs visual-activity off)
                     hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" }, -- render image files as images when opened
                 })
             end,
@@ -76,12 +76,55 @@ return {
         },
         config = function()
             require("neo-tree").setup({
+                commands = {
+                    copy_path = function(state)
+                        -- NeoTree is based on [NuiTree](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree)
+                        -- The node is based on [NuiNode](https://github.com/MunifTanjim/nui.nvim/tree/main/lua/nui/tree#nuitreenode)
+                        local node = state.tree:get_node()
+                        local filepath = node:get_id()
+                        local filename = node.name
+                        local modify = vim.fn.fnamemodify
+
+                        local results = {
+                            filepath,
+                            modify(filepath, ":."),
+                            modify(filepath, ":~"),
+                            filename,
+                            modify(filename, ":r"),
+                            modify(filename, ":e"),
+                        }
+
+                        vim.ui.select({
+                            "1. Absolute path: " .. results[1],
+                            "2. Path relative to CWD: " .. results[2],
+                            "3. Path relative to HOME: " .. results[3],
+                            "4. Filename: " .. results[4],
+                            "5. Filename without extension: " .. results[5],
+                            "6. Extension of the filename: " .. results[6],
+                        }, { prompt = "Choose to copy to clipboard:" }, function(choice)
+                            if choice then
+                                local i = tonumber(choice:sub(1, 1))
+                                if i then
+                                    local result = results[i]
+                                    vim.fn.setreg('*', result)
+                                    vim.notify("Copied: " .. result)
+                                else
+                                    vim.notify("Invalid selection")
+                                end
+                            else
+                                vim.notify("Selection cancelled")
+                            end
+                        end)
+                    end,
+                },
                 window = {
                     mappings = {
                         ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
+                        ["Y"] = "copy_path",
                     },
                 },
             })
+
             -- vim.keymap.set("n", "<C-b>", ":Neotree filesystem reveal left toggle<CR>", {})
             vim.api.nvim_set_keymap(
                 "n",
