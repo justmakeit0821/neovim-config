d1be82b0fb00fb9682aaba37cbcb60317828db92
Allen <allenhoproject@gmail.com>
2024-05-25 17:05:56 +0800

    [Plugin] add Copilot & Copilot Chat

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,38 @@
+return {
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        branch = "canary",
+        dependencies = {
+            {
+                "zbirenbaum/copilot.lua",
+                config = function()
+                    require("copilot").setup({})
+                end,
+            },
+            { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+        },
+        opts = {
+            debug = true,
+        },
+        keys = {
+            {
+                "<leader>ccq",
+                function()
+                    local input = vim.fn.input("Quick Chat: ")
+                    if input ~= "" then
+                        require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+                    end
+                end,
+            },
+            {
+                -- show prompts actions with telescope
+                "<leader>ccp",
+                function()
+                    local actions = require("CopilotChat.actions")
+                    require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+                end,
+                desc = "CopilotChat - Prompt Actions",
+            },
+        },
+    },
+}
