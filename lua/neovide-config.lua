582382d56ee1c725a50d7a8b86cabb6183b1ec52
Allen <allenhoproject@gmail.com>
2024-06-16 10:49:42 +0800

    [Neovide Config] update default font

--- lua/neovide-config.lua
+++ lua/neovide-config.lua
@@ -1,4 +1,4 @@
 vim.g.neovide_transparency = 0.8
--- vim.o.guifont = "PlemolJP35 Console NF Medium:h15"
-vim.o.guifont = "JetBrainsMono NF:h15"
+vim.o.guifont = "PlemolJP35 Console NF Medium:h15"
+-- vim.o.guifont = "JetBrainsMono NF:h15"
 -- vim.g.neovide_scale_factor = 1.2
